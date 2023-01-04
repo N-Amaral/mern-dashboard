46487cfb87ec21523e442412bf74ad42ca38ba99
NAmaral <26823958+N-Amaral@users.noreply.github.com>
2023-01-04 20:21:00 +0000

    Transactions Model, Route and Page

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,6 +1,7 @@
 import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
 import User from "../models/User.js";
+import Transaction from "../models/Transaction.js";
 
 export const getProducts = async (req, res) => {
   try {
@@ -29,6 +30,47 @@ export const getCustomers = async (req, res) => {
   try {
     const customers = await User.find({ role: "user" }).select("-password"); //select excludes password info from being returned to the frontend
     res.status(200).json(customers);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getTransactions = async (req, res) => {
+  try {
+    //deestructure page settings, sorting and size options from request
+    const { page = 1, pageSize = 20, sort = null, search = "" } = req.query;
+
+    const generateSort = () => {
+      const sortParsed = JSON.parse(sort);
+      const sortFormatted = {
+        [sortParsed.field]: (sortParsed.sort = "asc" ? 1 : -1),
+      };
+
+      return sortFormatted;
+    };
+    //check if sort exists otherwise returns default empty value
+    const sortFormatted = Boolean(sort) ? generateSort() : {};
+
+    const transactions = await Transaction.find({
+      $or: [
+        //query based on user input (optional input) Limited to only these two columns
+        { cost: { $regex: new RegExp(search, "i") } },
+        { userId: { $regex: new RegExp(search, "i") } },
+      ],
+    })
+      //sorts  based on user choice
+      .sort(sortFormatted)
+      //allows user to skip to certain page
+      .skip(page * pageSize)
+      //limits number of results
+      .limit(pageSize);
+
+    //returns full number
+    const total = await Transaction.countDocuments({
+      name: { $regex: search, $options: "i" },
+    });
+
+    res.status(200).json({ transactions, total });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
